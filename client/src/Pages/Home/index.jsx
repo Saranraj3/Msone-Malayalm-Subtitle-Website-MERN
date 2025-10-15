2018bf2ce5d0fed38354831dcf53f7952b954f03
Saran Raj <www.saranrajcoder@gmail.com>
2025-10-15 21:30:46 +0530

    feat: Home page background image added

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar'
-import BannerSlider from '../../Components/BannerSlider'
+import React from "react";
+import Navbar from "../../Components/Navbar";
+import BannerSlider from "../../Components/BannerSlider";
+import home from "../../Assets/Background/Home.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
-      <BannerSlider/>
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col relative"
+      style={{ backgroundImage: `url(${home})` }}
+    >
+      <Navbar />
+      <BannerSlider />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
